5607cf49a11dbba23948a556b163bab8f03628ac
aarushsingh142 <aarushsingh142@gmail.com>
2020-06-03 14:23:58 +0530

    schema created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();                      //middleware
 const port = process.env.port || 5000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true } );
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection is established.');
+});
 
 app.use(cors());
 app.use(express.json());
