f7ca4a88dd014e3593a1f83d85c16e62b0c549d6
aarushsingh142 <aarushsingh142@gmail.com>
2020-06-12 23:26:35 +0530

    adding frontend

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ListExercise extends Component {
+    render(){
+        return (
+            <div>
+                <p>This is the List Exercise Component</p>
+            </div>
+        )
+    }
+}
